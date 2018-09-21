e15ef2fcf981856530e8b0c4745a388ba402a67f
Marlon Mendes <marlonciriatico@gmail.com>
2018-09-21 02:11:25 -0300

    Comparing API az,el

--- tle_getter.py
+++ tle_getter.py
@@ -34,6 +34,26 @@ def get_tle(norad_id=None, name=None):
     line1, line2 = tle.split('\r\n')
     return line1, line2
 
+def get_az_el(lat, lng, alt, norad_id=None, name=None, seconds=1):
+    '''Gets satellite's AZ/EL from it's NORAD id:
+    https://en.wikipedia.org/wiki/Satellite_Catalog_Number'''
+
+    if norad_id is None:
+        if name is not None:
+            norad_id = SATELLITES_IDS[name]
+        else:
+            raise ValueError('NORAD id or satellite\'s name must be given')
+
+    API_NAME = 'N2YO'
+    ENDPOINT = 'https://www.n2yo.com/rest/v1/satellite/positions/{}/{}/{}/{}/{}/&apiKey={}'
+
+
+    url = ENDPOINT.format(norad_id, lat, lng, alt, seconds, API_KEYS[API_NAME])
+    response = requests.get(url).json()
+    positions = response['positions']
+    return positions[0]['azimuth'], positions[0]['elevation']
+
+
 if __name__ == '__main__':
     print('API_KEYS = {}'.format(API_KEYS))
 
